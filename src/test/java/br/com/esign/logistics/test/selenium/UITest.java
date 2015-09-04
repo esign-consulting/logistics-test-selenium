a647bd960b2990fb174aac2f52ba40216be21d26
Gustavo Muniz do Carmo <gustavo@esign.com.br>
2015-09-03 23:23:38 -0300

    Refactoring

--- src/test/java/br/com/esign/logistics/test/selenium/UITest.java
+++ src/test/java/br/com/esign/logistics/test/selenium/UITest.java
@@ -1,10 +1,33 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/* 
+ * The MIT License
+ *
+ * Copyright 2015 Esign Consulting Ltda..
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
 package br.com.esign.logistics.test.selenium;
 
+import br.com.esign.logistics.test.selenium.page.AddRouteModal;
+import br.com.esign.logistics.test.selenium.page.RemoveMapModal;
+import br.com.esign.logistics.test.selenium.page.BestRouteModal;
+import br.com.esign.logistics.test.selenium.page.AddMapModal;
+import br.com.esign.logistics.test.selenium.page.HomePage;
 import java.util.concurrent.TimeUnit;
 import org.junit.AfterClass;
 import org.junit.Test;
@@ -30,6 +53,9 @@ public class UITest {
     private final String PLACEB = "B";
     private final String PLACED = "D";
     
+    /**
+     * Starts the driver and opens the homepage.
+     */
     @BeforeClass
     public static void setup() {
         driver = new FirefoxDriver();
@@ -38,6 +64,9 @@ public class UITest {
         home = new HomePage(driver);
     }
     
+    /**
+     * Adds a map.
+     */
     @Test
     public void testA() {
         home.clickOnAddMapButton();
@@ -49,6 +78,9 @@ public class UITest {
         assertTrue(home.isAlertAddMapSuccessMessage(MAP));
     }
     
+    /**
+     * Checks the unique map constraint.
+     */
     @Test
     public void testB() {
         home.clickOnAddMapButton();
@@ -60,6 +92,9 @@ public class UITest {
         assertTrue(home.isAlertMapAlreadyExistsErrorMessage());
     }
     
+    /**
+     * Adds routes to the map.
+     */
     @Test
     public void testC() {
         home.selectMap(MAP);
@@ -85,6 +120,9 @@ public class UITest {
         assertTrue(home.isAlertAddRouteSuccessMessage(PLACEB, PLACED));
     }
     
+    /**
+     * Checks the unique route constaint.
+     */
     @Test
     public void testD() {
         home.clickOnAddRouteButton();
@@ -98,6 +136,9 @@ public class UITest {
         assertTrue(home.isAlertRouteAlreadyExistsErrorMessage());
     }
     
+    /**
+     * Checks the best route functionality.
+     */
     @Test
     public void testE() {
         home.clickOnBestRouteButton();
@@ -112,6 +153,9 @@ public class UITest {
         assertTrue(home.isAlertBestRouteSuccessMessage("A -> B -> D", "6.25"));
     }
     
+    /**
+     * Removes the map.
+     */
     @Test
     public void testF() {
         home.clickOnRemoveMapButton();
@@ -122,6 +166,9 @@ public class UITest {
         assertTrue(home.isAlertRemoveMapSuccessMessage(MAP));
     }
     
+    /**
+     * Closes the driver.
+     */
     @AfterClass
     public static void close() {
         driver.close();
---
+++ src/test/java/br/com/esign/logistics/test/selenium/page/RemoveMapModal.java
@@ -0,0 +1,65 @@
+/* 
+ * The MIT License
+ *
+ * Copyright 2015 Esign Consulting Ltda..
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package br.com.esign.logistics.test.selenium.page;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+/**
+ *
+ * @author gustavomunizdocarmo
+ */
+public class RemoveMapModal {
+
+    private final WebDriver driver;
+
+    @FindBy(tagName = "h3")
+    private WebElement heading;
+
+    @FindBy(name = "removeMapYesButton")
+    private WebElement removeMapYesButton;
+
+    @FindBy(name = "removeMapNoButton")
+    private WebElement removeMapNoButton;
+
+    public RemoveMapModal(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public boolean isPageOpened(String map) {
+        return heading.getText().contains(map.concat(": Remove Map"));
+    }
+    
+    public void clickOnRemoveMapYesButton() {
+        removeMapYesButton.click();
+    }
+    
+    public void clickOnRemoveMapNoButton() {
+        removeMapNoButton.click();
+    }
+
+}
