327f78cac2d4841d7d10cc3e84eb42532ad67e75
Gustavo Muniz do Carmo <gustavo@esign.com.br>
2015-09-03 23:56:38 -0300

    Refactoring

--- src/test/java/br/com/esign/logistics/test/selenium/page/RemoveMapModal.java
+++ src/test/java/br/com/esign/logistics/test/selenium/page/RemoveMapModal.java
@@ -1,7 +1,7 @@
 /* 
  * The MIT License
  *
- * Copyright 2015 Esign Consulting Ltda..
+ * Copyright 2015 Esign Consulting Ltda.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
